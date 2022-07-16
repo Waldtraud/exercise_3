d92bcd4afe193b984ee662b7ff4f1a9190160612
Waldtraud <50928003+Waldtraud@users.noreply.github.com>
2022-07-16 10:25:24 +0300

    Add project files.

---
+++ exercise/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ exercise/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise
+{
+    abstract class User
+    {
+        public string Name { get; private set; }
+        public int Damage { get; private set; }
+
+    }
+}
